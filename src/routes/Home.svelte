b80534ad3f326510d994eefff6d8f2f47a17fbba
veermistry <veermistry111222@gmail.com>
2021-04-19 14:31:53 -0500

    more candidate info & google maps function

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -30,7 +30,7 @@
             {latitude: location.latitude, longitude: location.longitude }
         )).toFixed(1);
     };
-	onMount( async () => {
+	/*onMount( async () => {
         // attempt to get user's geolocation, otherwise use the default for McKinney
         navigator.geolocation.getCurrentPosition(
             (position) => {
@@ -42,7 +42,7 @@
         );
 
         alert( `You are ${getDistanceInMiles()} miles away from the 1st voting location` );
-    });
+    });*/
 
 
 	let closestDist = 10000000000000000;
@@ -126,14 +126,65 @@
 					<script>
 						function initMap(){
 							var options = {
-								zoom:10,
+								zoom:9,
 								center:{lat:33.1983,lng:-96.6389}
 							}
 
 							var map = new 
 							google.maps.Map(document.getElementById('map'), options);
 
-							addMarker({coords:{lat: 33.346207, lng: -96.547211}});
+							addMarker({
+								coords:{lat: 33.346207, lng: -96.547211},
+								content: '<small>Anna ISD Administration Building</small>'
+							});
+							addMarker({
+								coords:{lat:33.30546, lng: -96.40425},
+								content: '<small>Blue Ridge ISD Administration</small>'
+							});
+							addMarker({
+								coords:{lat:33.32303, lng:-96.78852},
+								content: '<small>Celina ISD Administration Building</small>'
+							});
+							addMarker({
+								coords:{lat:33.21976, lng:-96.62817},
+								content: '<small>Collin County Elections Office</small>'
+							});
+							addMarker({
+								coords:{lat:33.16128, lng:-96.35975},
+								content: '<small>Farmersville City Hall</small>'
+							});
+							addMarker({
+								coords:{lat:33.28182, lng: -96.5667},
+								content: '<small>First Melissa</small>'
+							});
+							addMarker({
+								coords:{lat:33.06636, lng: -96.43309},
+								content: '<small>Josephine City Hall</small>'
+							});
+							addMarker({
+								coords:{lat:33.02636, lng:-96.56556},
+								content: '<small>Lavon City Hall</small>'
+							});
+							addMarker({
+								coords:{lat:33.21091, lng:-96.56556},
+								content: '<small>New Hope Town Hall</small>'
+							});
+							addMarker({
+								coords:{lat:33.18083, lng: -96.49537},
+								content: '<small>Princeton Public Works Department</small>'
+							});
+							addMarker({
+								coords:{lat:33.23468, lng:-96.8039},
+								content: '<small>Prosper Town Hall</small>'
+							});
+							addMarker({
+								coords:{lat:33.34722, lng:-96.66892},
+								content: '<small>Weston Community Center</small>'
+							});
+							addMarker({
+								coords:{lat:33.0038, lng: -96.53215},
+								content: '<small>Wylie Senior Recreation Center</small>'
+							});
 
 							function addMarker(props){
 								var marker = new google.maps.Marker({
@@ -141,35 +192,20 @@
 								map:map 
 							});
 							}
-							/*marker1 = new google.maps.Marker({
-								position:{lat: 33.346207, lng: -96.547211},
-								map:map
-							});
-							var infoWindow = new google.maps.InfoWindow({
-								content: '<a href={locations.MapsLink}></a>'
-							});
-							marker1.addListener('click', function)
-							marker2 = new google.maps.Marker({
-								position:{lat:33.30546, lng: -96.40425},
-								map:map
-							});
-							
 
-							marker3 = new google.maps.Marker({
-								position:{lat:33.32303, lng:-96.78852},
-								map:map
-							});
-							
+							if(props.iconImage){
+								marker.setIcon(props.iconImage);
+							}
 
-							marker4 = new google.maps.Marker({
-								position:{lat:33.32303, lng:-96.78852},
-								map:map
-							});
-							
-							marker5 = new google.maps.Marker({
-								position:{lat:33.16128,lng:-96.35975},
-								map:map
-							});*/
+							if(props.content){
+								var infoWindow = new google.maps.infoWindow({
+									content:props.content
+								});
+
+								marker.addListener('click', function(){
+									infoWindow.open(map, marker);
+								});
+							}
 
 						}
 					</script>
@@ -181,15 +217,11 @@
                 <div class="grid mx-5 md:grid-cols-3">
 					<div class="block">
                         <div class="ml-5">
-								<span class="text-gray-700 font-main">Location QuickSearch:</span>
+								<span class="text-gray-700 font-main">Location QuickSearch:<small class="mx-2 leading-tight">If there is a location your zip code, it will appear below.</small></span>
 								<input on:keytype={ZIP} type="text" class="form-input block mt-1 w-56 border border-blue-900" placeholder="Insert ZIP" bind:value={ZIP}>
 								{#each locations as location}
 									{#if ZIP == location.Zip}
 										<span class="block"><a href={location.MapsLink} target="_blank">Location: <u>{location.Name}, {location.Room}</u></a>
-										<p>This location is {geolib.getDistance(
-											{latitude: location.Latitude, longitude: location.Longitude},
-											{latitude: 40.7580, longitude: 73.9855}
-										)} meters from Times Square NYC!</p>
 										</span>
 									{/if}
 								{/each}
@@ -237,7 +269,7 @@
 						<small class="text-gray-500">Click on their names to go to their webpages.</small>
 					</p>
 				</div>
-				<div class="grid md:grid-cols-3 md:grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 rounded-md overflow-hidden gap-10 lg:gap-20 px-5">
+				<div class="grid md:grid-cols-2 md:grid-rows-1 rounded-md overflow-hidden gap-10 lg:gap-20 px-5">
 					<div class="bg-gray-200 shadow-md rounded-lg overflow-hidden">
 						<img src="img/George_Fuller.jpeg" alt="George Fuller" class="w-full h-60 object-cover content-center shadow-md">
 						<div>
@@ -248,19 +280,31 @@
 						</div>
 					</div>
 					<div class="bg-gray-200 rounded-lg overflow-hidden shadow-xl">
-						<img src="img/Jimmy_Stewart.jpeg" alt="Jimmy Stewart" class="w-full h-60 object-cover content-center shadow-md">
-						<div>
-							<span class=" font-bold text-center mx-2" ><a href="https://www.facebook.com/Jimmy-Stewart-For-McKinney-Mayor-353532622386631" target="_blank" class="underline">Jimmy Stewart</a></span>
-							<span class="block leading-tight mx-3">George info</span>
-						</div>
-					</div>
-					<div class="bg-gray-200 rounded-lg overflow-hidden shadow-xl">
 						<img src="img/Thomas_Meredith.jpeg" alt="Thomas_Meredith" class="w-full h-60 object-cover content-center shadow-md">
 						<div>
 							<span class="font-bold text-center mx-2" > <a href="https://www.facebook.com/Meredith-For-McKinney-104874304725724" target="_blank" class="underline">Thomas Meredith</a> </span>
-							<span class="block leading-tight mx-3">George info</span>
+							<span class="block leading-tight mx-3">Thomas Meredith is a small business owner and active citizen & participant in local politics. 
+								He and his wife have owned and managed The Pantry restaurant in downtown McKinney for the past 20 years, which was recognized for Outstanding Small Business twice. 
+								Tom has worked with fellow McKinney business owners to propose such-minded policies to the McKinney municipal government, including implementing 3 hour parking downtown and adding lights in the parking as well. 
+								Tom has been involved in local elections for many years, has been on the City of McKinney bond & McKinney Census Community in 2010, and was Vice President & Treasurer of the Property Owners organization from 2003–2009. 
+								Tom is a father and is married to Cleo Meredith.</span>
 						</div>
 					</div>
+				</div>
+				<div class="ml-4 mt-3 mx-5"> 
+					<p class="text-xl">
+						Incumbent Mayor Fuller notable achievements & occurrences:
+					</p>
+					<p class="block text-base"> 
+						• 2021 was the first year for McKinney to have no-new-revenue tax rate in recent history.
+						<br> • McKinney recieved a Cultural District Designation from the Texas Commission on the Arts.
+						<br> • McKinney also recieved designation as a 'Texas Music Friendly Community' from the state government.
+						<br> • Amazon & Raytheon expanded in McKinney greatly.
+						<br> • First public parking garage was built in Downtown McKinney.
+						<br> • Independent Financial headquarters are being built in McKinney.
+						<br> • Turned the McKinney National Airport profitable while eliminating it's subsidies
+						<br> • Began construction of a new FBO Terminal at the airport.
+					</p>
 				</div>
 				<div class="p-6 text-3xl text-center">
 					{val_tax} 
@@ -274,16 +318,27 @@
 				</div>
 				<div class="grid grid-rows-3 grid-cols-2 lg:grid-cols-5 lg:grid-rows-1 rounded-md overflow-hidden gap-5 px-5">
 					<div class="bg-gray-200 shadow-xl rounded-md">
-						<span class=" font-bold text-center mx-2" ><a href="https://www.facebook.com/Jimmy-Stewart-For-McKinney-Mayor-353532622386631" target="_blank" class="underline">Cristoval (Cris) Treviño</a></span>
-							<span class="block leading-tight mx-3">George info</span>
+						<span class=" font-bold text-center mx-2" ><a href="https://www.facebook.com/Cris-Trevino-for-McKinney-D1-City-Council-109026511061328/?ref=page_internal" target="_blank" class="underline">Cristoval (Cris) Treviño</a></span>
+							<span class="block leading-tight mx-3">Cris Treviño is a born-and-raised McKinney citizen who has worked in law enforcement for the past 19 years. 
+								He grew up in District 1, and has been involved in the community for years. 
+								Cris looks to add a Neighborhood Walmart & pharmacy, more banking services, a mini postal location, and create shuttles to McKinney medical services.
+								Cris also plans on creating a District 1 business assocation, for business owners in the district to discuss issues and work towards mutual success.
+							</span>
 					</div>
 					<div class="bg-gray-200 shadow-xl rounded-md">
-						<span class=" font-bold text-center mx-2" ><a href="https://www.facebook.com/Jimmy-Stewart-For-McKinney-Mayor-353532622386631" target="_blank" class="underline">Johnny Moore</a></span>
-							<span class="block leading-tight mx-3">George info</span>
+						<span class=" font-bold text-center mx-2" ><a href="https://www.facebook.com/Moore.Johnny12/" target="_blank" class="underline">Johnny Moore</a></span>
+							<span class="block leading-tight mx-3">Johnny Moore is a high school counselor and has coached high school football & track for 13 years. 
+								Johnny prioritizes the future leaders of McKinney, as looks to work in collaboration with Collin College to subsidize tuition costs.
+								Johnny is married and has a daughter who have lived in McKinney for many years.
+							</span>
 					</div>
 					<div class="bg-gray-200 shadow-xl rounded-md">
-						<span class=" font-bold text-center mx-2" ><a href="https://www.facebook.com/Jimmy-Stewart-For-McKinney-Mayor-353532622386631" target="_blank" class="underline">Justin Beller</a></span>
-							<span class="block leading-tight mx-3">George info</span>
+						<span class=" font-bold text-center mx-2" ><a href="https://votebeller.com/" target="_blank" class="underline">Justin Beller</a></span>
+							<span class="block leading-tight mx-3">Justin Beller is a community banker and active citizen & participant in the community.
+								He is and has been involved in many community organizations, serving on the board of the Holy Family School in 2011, and was appointed by the city government to serve on the McKinney Housing Authority Board for 6 years, redeveloping Newsome and Merritt homes.
+								During his membership on the housing board, Justin started a group for young men called McKinney Force, building relationships & community with District 1 residents, with an emphasis on civic duty and empowerment. 
+								Justin has also served on the Chamber of Commerce Board, and was recognized as the leadership alumni of the year. 
+							</span>
 					</div>
 					<div class="bg-gray-200 shadow-xl rounded-md">
 						<span class=" font-bold text-center mx-2" ><a href="https://www.facebook.com/Jimmy-Stewart-For-McKinney-Mayor-353532622386631" target="_blank" class="underline">Stanley Penn</a></span>
