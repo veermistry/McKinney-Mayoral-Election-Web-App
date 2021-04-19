21923dfdc797c80b7bcf4e9b6d45a9b8c5be2426
veermistry <veermistry111222@gmail.com>
2021-04-19 14:52:46 -0500

    call test

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -391,6 +391,9 @@
 </main>
 
 <svelte:head>
+	<script defer async
+					src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJ1Z1ZXwvhBxVWmdzGB22X5gE5VaOOwoM&callback=initMap&libraries=&v=weekly">
+		</script>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
