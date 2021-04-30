3a26b9735f503a7a22e556de309c3959c3e9ec1d
veermistry <veermistry111222@gmail.com>
2021-04-30 00:40:02 -0500

    redefining current position

--- src/resources/views/Home.svelte
+++ src/resources/views/Home.svelte
@@ -8,7 +8,7 @@
 
 	let val_tax = 20;
   $: closestLocation = locations[2];
-  let currentPosition = pos.coords;
+  let currentPosition = navigator.geolocation.getCurrentPosition(onLoadCurrentPosition, onErrorLoadingCurrentPosition);
 //let currentPosition = { latitude: 0.000, longitude: 0.000};
 	let closestDist = 10000000000000000;
 	let showModal = false;
